2443adc9c36a23e8b292e0af55b89653f1ddbfc2
V N <vee.vidify@gmail.com>
2021-10-23 00:12:20 +1100

    add example for middleware

--- examples/middleware_and_cors.py
+++ examples/middleware_and_cors.py
@@ -1,23 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException, status, Request
-from fastapi.middleware.cors import CORSMiddleware
 
 import time
 
 app = FastAPI()
-
-allowed_origins = [
-    "http://localhost:8080",
-    "http://localhost:8000",
-    "http://localhost",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=allowed_origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "OPTIONS", "PATCH", "DELETE"],
-    allow_headers=["*"]
-)
 
 @app.middleware("http")
 async def process_time_header(request: Request, next):
